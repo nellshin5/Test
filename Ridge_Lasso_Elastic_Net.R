3799b3a7c3f855cdfbf922e8b843031e18157e2f
nellshin5 <77715555+nellshin5@users.noreply.github.com>
2022-03-28 20:12:34 -0500

    sample 파일 업로드
    sample 파일 업로드

---
+++ Ridge_Lasso_Elastic_Net.R
@@ -0,0 +1,22 @@
+install.packages("ISLR")
+install.packages("glmnet")
+library(glmnet)
+
+data(Hitters, package = "ISLR")
+head(Hitters)
+dim(Hitters)
+
+#missing data check
+sum(is.na(Hitters$Salary))
+Hitters = na.omit(Hitters)
+sum(is.na(Hitters))
+names(Hitters)
+
+X = model.matrix(Salary ~ ., Hitters)[, -1]
+y = Hitters$Salary
+
+fit = lm(Salary ~ ., Hitters)
+coef(fit)
+
+sum(abs(coef(fit)[-1])) #intercept ª©∞Ì predictor coefficient ¥Ÿ ¥ı«‘
+sum(coef(fit)[-1] ^ 2)
